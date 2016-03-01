1c2924a7b562e66f29443e32ac0df04da3b8ff0a
melissathompson24 <melissa.thompson24@gmail.com>
2016-03-01 13:05:02 -0700

    update toolkit with search

---
+++ js/load_essentials.js
@@ -0,0 +1,81 @@
+// navigation 
+
+document.getElementById("myNav").innerHTML =
+  "<div class='container-fluid'>"
+  +  "<div class='search pull-right'><a href='search.html'><h3><span class='glyphicon glyphicon-search'></span></h3></div>"
+  +  "<div class='navbar-header'>"
+  +  "<a class='navbar-brand' href='index.html'>"
+  +  "<img alt='NCWWI Tool Kit Brand: Pathway leading to a Tree' src='./images/brand.png'>"
+  +  "</a>"
+  +  "<a href='#skipToContent'>"
+  +  "<img class='img-responsive' alt='Skip to main content' src='./images/skipToContent.png'>"
+  +  "</a>"
+  +  "<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>"
+  +  "<span class='sr-only'>Toggle navigation</span>"
+  +  "<span class='icon-bar'></span>"
+  +  "<span class='icon-bar'></span>"
+  +  "<span class='icon-bar'></span>"
+  +  "</button>"
+  +  "</div>"
+  +  "<div id='navbar' class='collapse navbar-collapse  navbar-right''>"
+  +  "<ul class='nav navbar-nav'>"
+  +  "<li class='active'><a href='index.html'>Home</a></li>"
+  +  "<li class='dropdown'>"
+  +  "<a href='why.html' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Why <span class='caret'></span></a>"
+  +  "<ul class='dropdown-menu'>"
+  +  "<li><a href='why.html'>Leadership is Essential</a></li>"
+  +  "<li><a href='why.html#framework'>Leadership Model & Framework</a></li>"
+  +  "<li><a href='why.html#need'>The Need for a Tool Kit</a></li>"
+  +  "<li><a href='why.html#ncwwi'>NCWWI</a></li>"
+  +  "<li><a href='why.html#acknowledgements'>Acknowledgements</a></li>"
+  +  "</ul>"
+  +  "</li>"
+  +  "<li><a href='who.html'>Who</a></li>"
+  +  "<li  class='dropdown'>"
+  +  "<a href='how.html' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>How <span class='caret'></span></a>"
+  +  "<ul class='dropdown-menu'>"
+  +  "<li><a href='how.html'>Using the Tool Kit</a></li>"
+  +  "<li><a href='how.html#ldp'>Leadership Development Plan</a></li>"
+  +  "</li>"
+  +  "</ul>"
+  +  "<li class='dropdown'>"
+  +  "<a href='domains.html' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Tool Kit <span class='caret'></span></a>"
+  +  "<ul class='dropdown-menu'>"
+  +  "<li><a href='domains.html'>Leadership Domains</a></li>"
+  +  "<li><a href='domain1.html'>1: Leading Change</a></li>"
+  +  "<li><a href='domain2.html'>2: Leading People</a></li>"
+  +  "<li><a href='domain3.html'>3: Leading for Results</a></li>"
+  +  "<li><a href='domain4.html'>4: Leading in Context</a></li>"
+  +  "<li><a href='domain5.html'>5: Fundamental Competencies</a></li>"
+  +  "<li><a href='references.html'>References</a></li>"
+  +  "</ul>"
+  +  "</li>"
+  +  "</ul>"
+  +  "<ul class='nav navbar-nav'>"
+  +  "<li><a href='download.html'>Download</a></li>"
+  +  "<li><a href='contact.html'>Contact</a></li>"
+  +  "<li><a href='sitemap.html'>Sitemap</a></li>"
+  +  "</ul>"
+  +  "</div><!--/.nav-collapse -->"
+  +  "</div>"
+  ;
+
+
+
+
+
+document.getElementById("myFooter").innerHTML =
+  "<div class='container-fluid text-center'>"
+  +  "<p class='text-muted'><a href='index.html'>Learning \& Living Leadership: A Tool Kit</a></p>"
+  +  "<p class='text-muted'><a href='http://www.ncwwi.org'>National Child Welfare Workforce Institute</a></p>"
+  +  "<p class='text-muted'><a href='http://www.ncwwi.org'>www.NCWWI.org</a> | <a href='http://www.myncwwi.org'>www.MyNCWWI.org</a></p>"
+  +  "<p class='text-muted'>Copyright &copy; " 
+  +  new Date().getFullYear()
+  +  ". All Rights Reserved.</span></p>"
+  +  "<img src='./images/NCWWI-logo_white_100px.png' alt='National Child Welfare Workforce Institute. Learning, Leading, Changing.'>"
+  +  "</div>"
+  ;
+
+
+
+
